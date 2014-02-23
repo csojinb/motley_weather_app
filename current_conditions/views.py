fc36f85b18153a42608a8408b9fed0e96b13019d
Clara Bennett <csojinb@gmail.com>
2014-02-23 21:58:01 +0900

    Reorganized current_conditions.views so that the default zip code is no longer required. Changed formatting of ValidationError output for zip_form.

--- current_conditions/views.py
+++ current_conditions/views.py
@@ -13,32 +13,37 @@ def index(request):
         zip_form = LocationForm(request.POST)
         if zip_form.is_valid():
             zip_code = zip_form.cleaned_data['zip_code']
-        
+      
+            request_url = wunderground_url_base + str(zip_code) + '.json'
+
+            r = requests.get(request_url)
+            r = r.json()
+
+            try:
+                conditions = {'Temperature' : r['current_observation']['temperature_string'],
+                                'Feels Like' : r['current_observation']['feelslike_string'],
+                }
+                location = r['current_observation']['observation_location']['full']
+
+                return render(request, 'current_conditions/index.html', {'location' : location, 
+                                                                    'conditions' : conditions,
+                                                                    'zip_form' : zip_form,
+                                                                    })
+            except:
+                error_msg = r['response']['error']['description']
+
+                return render(request, 'current_conditions/index.html', {'zip_form': zip_form,
+                                                                        'error_msg' : error_msg,
+                                                                        })
+
     else:
         zip_form = LocationForm()
 
+    return render(request, 'current_conditions/index.html', {'zip_form': zip_form,
+                                                                    })
+        
 
-    request_url = wunderground_url_base + str(zip_code) + '.json'
-
-    r = requests.get(request_url)
-    r = r.json()
-
-    try:
-        conditions = {'Temperature' : r['current_observation']['temperature_string'],
-                        'Feels Like' : r['current_observation']['feelslike_string'],
-        }
-        location = r['current_observation']['observation_location']['full']
-
-        return render(request, 'current_conditions/index.html', {'location' : location, 
-                                                            'conditions' : conditions,
-                                                            'zip_form' : zip_form,
-                                                            })
-    except:
-        error_msg = r['response']['error']['description']
-
-        return render(request, 'current_conditions/index.html', {'zip_form': zip_form,
-                                                                'error_msg' : error_msg,
-                                                                })
+       
 
     ## !!! Change this page around so that I don't need to start with a default zip code!!
     
--- weather/settings.py
+++ weather/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'current_conditions'
 )
 
 MIDDLEWARE_CLASSES = (
